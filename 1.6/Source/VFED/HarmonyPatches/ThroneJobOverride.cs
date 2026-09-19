6d50f3fdad842ec5ff3d60c9545e0110680b8bb9
chris-b-90 <kedjown@gmail.com>
2026-09-19 13:30:39 +0100

    patch thinknode to wrap an expiry

---
+++ 1.6/Source/VFED/HarmonyPatches/ThroneJobOverride.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Verse.AI;
+using Verse;
+ 
+namespace ShinobiFP.VFED.HarmonyPatches;
+ 
+[HarmonyPatch(typeof(Pawn_JobTracker), nameof(Pawn_JobTracker.JobTrackerTick))]
+public static class Patch_ForceThroneOverrideCheck
+{
+    public static void Postfix(Pawn_JobTracker __instance, Pawn ___pawn)
+    {
+        if (___pawn?.mindState?.duty?.def == global::VFED.VFED_DefOf.VFED_SitOnThrone
+            && ___pawn.IsHashIntervalTick(60))
+        {
+            __instance.CheckForJobOverride();
+        }
+    }
+}
--- 1.6/Source/VFED/ShinobiFP.VFED.cs
+++ 1.6/Source/VFED/ShinobiFP.VFED.cs
@@ -1,4 +1,6 @@
+using System;
 using Verse;
+using HarmonyLib;
 
 namespace ShinobiFP.VFED;
 
@@ -6,5 +8,14 @@ public class ShinobiFPVFEDMod : Mod
 {
     public ShinobiFPVFEDMod(ModContentPack content) : base(content)
     {
+        try
+        {
+            var harmony = new Harmony("ShinobiFP.VFED");
+            harmony.PatchAll();
+        }
+        catch (Exception e)
+        {
+            Log.Error("[Shinobi Flavour Pack] Failed to apply VFED compatibility patches: " + e);
+        }
     }
 }
